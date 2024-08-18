1557315446ab8e229fd4b9e79b92d487266e42b3
riyadh-radhi <riyadhradhi0@gmail.com>
2024-08-18 21:21:20 +0300

    Solve day one tasks

--- src/com/iraqidata/tasks/day_one.clj
+++ src/com/iraqidata/tasks/day_one.clj
@@ -4,9 +4,10 @@
 ;; 1. Write a function that takes one argument as input and prints that
 ;; argument.
 
+(defn print_basic [p] 
+  (println p))
 
-
-
+(print_basic "Hello")
 
 
 ;; 2. Write a function that adds `1` to a number only if the input is odd,
@@ -14,10 +15,11 @@
 
 ;; Tip, use the `odd?` function to check if a number is odd.
 
+(defn add_to_odd [n]
+  (if (odd? n) (+ n 1) :error))
 
-
-
-
+(add_to_odd 3)
+(add_to_odd 2)
 
 
 ;; 3. Write a function that takes 3 arguments, `name`, `year-of-birth`, and
@@ -26,9 +28,10 @@
 ;; Example run
 ;; (function-name "Ali" 2001 2024) => {:name "Ali", :age 23}
 
+(defn get_age [name year-of-birth current-year]
+  {:name name :age (- current-year year-of-birth)})
 
-
-
+(get_age "Adham" 2001 2024)
 
 ;; 4. Write a function that takes the output of the above function and returns
 ;; `true` if the person is allowed to vote (assume the voting age is 18).
@@ -38,8 +41,12 @@
 ;; (function-name "Ali" 2001 2024) => true
 ;; (function-name {:name "Abbas", :age 17}) => false
 
+(defn is_vote_allowed [demographic]
+  (if (> (get demographic :age) 18) true false))
 
-
+(is_vote_allowed {:name "Ali", :age 23})
+(is_vote_allowed (get_age "Ali" 2001 2024))
+(is_vote_allowed {:name "Abbas", :age 17})
 
 ;; OPTIONAL FOR BONUS POINTS
 
@@ -48,3 +55,11 @@
 ;; (function-name "Ali" 2001) => {:name "Ali", :age 23}
 ;; If ran in 2025
 ;; (function-name "Ali" 2001) => {:name "Ali", :age 24}
+
+
+;; Very dirty solution, sorry!
+
+(defn get_age_smart [name year-of-birth]
+  {:name name :age (- (Integer. (subs  (.toString (new java.util.Date)) 24)) year-of-birth)})
+
+(get_age_smart "Ali" 2001)
