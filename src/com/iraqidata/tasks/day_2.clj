6c902af5f9619282c21e5c1539ffd09e597d3c76
riyadh-radhi <riyadhradhi0@gmail.com>
2024-08-20 23:31:49 +0300

    Solve day-2 tasks

--- src/com/iraqidata/tasks/day_2.clj
+++ src/com/iraqidata/tasks/day_2.clj
@@ -9,16 +9,40 @@
                     {:key-fn #(keyword (str/replace (name %) "_" "-"))}))
 
 ;; 1. How many flights were there in total?
+;;;;;;;;;;;;;;;;;;
+(-> ds
+    tc/row-count)
 
+;; Each row represents a flight, 
+;; Total number of flights = Number of rows = 336776 
+;;;;;;;;;;;;;;;;;;
 
 ;; 2. How many unique carriers were there in total?
+;;;;;;;;;;;;;;;;;;
+(-> ds
+    (tc/unique-by  :carrier)
+    (tc/row-count))
 
+;; 16 unique carriers
+;;;;;;;;;;;;;;;;;;
 
 ;; 3. How many unique airports were there in total?
+;;;;;;;;;;;;;;;;;;
+(-> ds 
+    (tc/pivot->longer  [:origin :dest])
+    (tc/unique-by :$value)
+    (tc/row-count))
 
+;; 107 unique airports
+;;;;;;;;;;;;;;;;;;
 
 ;; 4. What is the average arrival delay for each month?
+;;;;;;;;;;;;;;;;;;
+(-> ds
+    (tc/group-by :month)
+    (tc/mean :arr-delay))
 
+;;;;;;;;;;;;;;;;;;
 
 ;; Optional: Use the `airlines` dataset to get the name of the carrier with the
 ;; highest average distance.
@@ -26,3 +50,14 @@
 (def airlines
   (tc/dataset "./resources/data/airlines.csv"
               {:key-fn keyword}))
+;;;;;;;;;;;;;;;;;;
+(-> ds
+    (tc/select-columns [:carrier :distance])
+    (tc/inner-join airlines :carrier)
+    (tc/group-by :name)
+    (tc/mean :distance)
+    (tc/order-by "summary" :desc)
+    (tc/head 1))
+
+;; Hawaiian Airlines Inc.
+;;;;;;;;;;;;;;;;;;
